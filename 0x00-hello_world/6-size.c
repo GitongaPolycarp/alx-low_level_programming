b2fa02e70565d41e12baa284dd59124b8aa99147
GitongaPolycarp <memepolycarp@gmail.com>
2023-10-06 01:17:50 +0300

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ *
+ * Return: 0 on succes
+ */
+
+int main(void)
+{
+	printf("Size of a char: %1u byte(s),sizeof(char));
+	printf("size of int: %1u byte(s)", sizeof(int));
+	printf("size of a long int: %1u byte(s)", sizeof(long int));
+	printf(size of a long long int: %1u byte(s)", sizeof(long long int))
+	printf(Size of a float: %1u byte(s)", sizeof(float));
+	return (0)
+		
+}	
