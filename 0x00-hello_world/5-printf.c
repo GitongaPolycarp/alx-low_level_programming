d1bbd4465e301a791e5412f64e9859fb09b6b30e
GitongaPolycarp <memepolycarp@gmail.com>
2023-10-07 20:37:54 +0300

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /**
  * main - main prints size
- * 
+ *
  * Return: 0 when succesfull
  */
 
-int main(void) 
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
