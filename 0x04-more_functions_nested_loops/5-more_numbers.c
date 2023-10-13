7ade62a924b0afe4f7b418aa17b16d90b2560823
GitongaPolycarp <memepolycarp@gmail.com>
2023-10-13 23:17:31 +0300

    5-main.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * more_numbers - prints 10 times the numbers from 0 to 14
- * Return: always 0
+ * Return: void
  */
 
 void more_numbers(void)
@@ -11,11 +11,10 @@ void more_numbers(void)
 	int j;
 
 	for (i = 0 ; i < 10 ; i++)
-
 	{
 		for (j = 0 ; j <= 14 ; j++)
 		{
-			if (j > 0)
+			if (j > 9)
 				_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
 		}
