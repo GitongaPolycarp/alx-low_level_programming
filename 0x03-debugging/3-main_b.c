685893c48c678652087765e4e0b786466726d5dd
GitongaPolycarp <memepolycarp@gmail.com>
2023-10-16 21:18:28 +0300

    0-main.c

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-int convert(int month, int day);
-void print_remaining_days(int month, int day, int year);
-
 /**
  * main - takes a date and prints how many days are left in the year, taking
  * leap years into account
@@ -21,10 +18,6 @@ int main(void)
 	year = 2000;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
-
-	day = convert_day(month, day);
-
-	print_remaining_days(month, day, year);
 
 	return (0);
 }
