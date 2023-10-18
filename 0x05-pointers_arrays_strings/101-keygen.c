ae35b517030da29b91912a2b5079313fbccd34bb
GitongaPolycarp <memepolycarp@gmail.com>
2023-10-18 18:11:54 +0300

    101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,7 +5,7 @@
 /**
  * main - program generates random valid passwors
  * passwords for program 101-crackme
- * Return: always 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -19,7 +19,7 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		pass[i] = rand() % 70;
+		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
 		if ((2772 - sum) - '0' < 78)
@@ -30,6 +30,7 @@ int main(void)
 			break;
 		}
 	}
+
 	return (0);
 }
 
