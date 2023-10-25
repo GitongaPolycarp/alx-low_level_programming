18e285a4ac33f26f2de9688bf4b258736381b61d
GitongaPolycarp <memepolycarp@gmail.com>
2023-10-25 20:15:28 +0300

    prints in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,17 +2,14 @@
 
 /**
  * _print_rev_recursion - prints a string in reverse.
- * @s: string to reverse
+ * @s: string to be reversed
  * Return: 0
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\n')
+	if (*s != '\0')
 	{
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	s++;
-	_print_rev_recursion(s);
-	s--;
-	_putchar(*s);
 }
