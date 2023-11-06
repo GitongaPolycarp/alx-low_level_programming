ddbb7103f9c9beac3d5aed6742eeb031034dd17e
GitongaPolycarp <memepolycarp@gmail.com>
2023-11-06 22:22:09 +0300

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new type describing dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
