63e0ec2a782b2ea1ca0e0d2a5c41a4f814d4c3a5
GitongaPolycarp <memepolycarp@gmail.com>
2023-11-17 21:55:41 +0300

    holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+section .data
+message db "Hello, Holberton\n"
+
+section .text
+global _start
+
+_start:
+mov rax, 1 ;
+mov rdi, 1 ;
+mov rsi, message ;
+mov rdx, 16 ;
+syscall ;
+mov rax, 60 ;
+xor rdi, rdi ;
+syscall ;
