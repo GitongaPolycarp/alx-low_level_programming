ac417c233199eaf7cbfd7f0c51d6b36d5504388f
GitongaPolycarp <memepolycarp@gmail.com>
2023-11-17 21:35:01 +0300

    add_node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,11 +9,10 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *newnode;
-	int i, count = 0;
+	list_t *newnode, *tmp;
+	unsigned int i, count = 0;
 
 	newnode = malloc(sizeof(list_t));
-
 	if (newnode == NULL)
 		return (NULL);
 	newnode->str = strdup(str);
@@ -21,6 +20,15 @@ list_t *add_node_end(list_t **head, const char *str)
 		count++;
 	newnode->len = count;
 	newnode->next = NULL;
+	tmp = *head;
 
+	if (tmp == NULL)
+		*head = newnode;
+	else
+	{
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = newnode;
+	}
 	return (*head);
 }
