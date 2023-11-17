5be5b6a73693be1408f8c400e3b4ef1aa669d754
GitongaPolycarp <memepolycarp@gmail.com>
2023-11-17 21:47:12 +0300

    printing

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
- *
+ * before_main - function that runs
  */
 
 void __attribute__ ((constructor)) before_main()
 {
-    printf("You're beat! and yet, you must allow,\n; I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
