74e932e16898a5facab0a4c16d0d3ab7c1f34297
GitongaPolycarp <memepolycarp@gmail.com>
2023-11-20 22:35:46 +0300

    print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,10 +9,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	if (h == NULL)
+		return (0);
+	for (count = 0; h != NULL; count++)
 	{
 		printf("%d\n", h->n);
-		count++;
 		h = h->next;
 	}
 
