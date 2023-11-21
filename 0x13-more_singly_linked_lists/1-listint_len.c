0bfd3902c4cbda9073e62c83cb450d1055ddb74a
GitongaPolycarp <memepolycarp@gmail.com>
2023-11-21 09:47:00 +0300

    returns no. of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in linked listint
+ * @h: nodes
+ *
+ * Return: number of elements in linked listimt
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+	}
+	return (count);
+}
