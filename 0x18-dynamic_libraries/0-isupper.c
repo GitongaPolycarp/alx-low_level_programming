64785be6e1c224df95a5fcbe362c890d72f1f302
POLYCARP <memepolycarp@gmail.com>
2024-01-15 23:52:40 +0300

    task 0

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - function chesks uppercase letters
+ * @c: char
+ *
+ * Return: if uppercase 1, else 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'E')
+		return (1);
+
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to concatenate
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = strlen(dest);
+	int i;
+
+	for (i = 0 ; i < n && *src != '\0' ; i++)
+	{
+		dest[index + i] = *src;
+		src++;
+	}
+	dest[index + i] = '\0';
+	return (dest);
+}
